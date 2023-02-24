98caeb05e57f3e6ec0655d0babe00d3bf1baaced
warmachine028 <warmachine028@outlook.com>
2023-02-24 23:38:49 +0530

    feat: Added Firebase Authentication and Firestore Database

---
+++ src/context/AuthContext.js
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState, useEffect } from "react"
+import {
+	onAuthStateChanged,
+	signOut, // To sign out user
+	signInWithEmailAndPassword, // To sign in user using Email and Password
+	createUserWithEmailAndPassword, // To create user using Email and Password
+	signInWithPopup,
+	GoogleAuthProvider, // Sign In with Google
+} from "firebase/auth"
+import { auth } from "../firebase"
+
+const AuthContext = createContext()
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState()
+	const [loading, setLoading] = useState(true)
+	const provider = new GoogleAuthProvider()
+
+	const signIn = (email, password) => {
+		return signInWithEmailAndPassword(auth, email, password)
+	}
+	const signInWithGoogle = () => {
+		return signInWithPopup(auth, provider)
+	}
+	const signUp = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password)
+	}
+
+	const logOut = () => {
+		localStorage.removeItem("user")
+		signOut(auth)
+	}
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, user => {
+			setCurrentUser(user)
+			setLoading(false)
+		})
+		return unsubscribe
+	}, [])
+
+	const value = {
+		currentUser,
+		signUp,
+		signIn,
+		signInWithGoogle,
+		logOut,
+	}
+	return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>
+}
+
+export default AuthProvider

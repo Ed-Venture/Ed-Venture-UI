dc2da3b37a6e4c61fa33f07800a2c87f3eeee115
warmachine028 <warmachine028@outlook.com>
2023-02-25 15:18:10 +0530

    fix: Removed console warnings about key()

--- src/pages/Classes/Class.jsx
+++ src/pages/Classes/Class.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { ClassData } from "./ClassData";
-import { NavLink } from "react-router-dom";
+import React from "react"
+import { ClassData } from "./ClassData"
+import { NavLink } from "react-router-dom"
 
 export default function Class() {
-  return (
-    <div className="container flex flex-wrap">
-      {ClassData.map((user) => (
-        <NavLink to="/Class/:id/stream" style={{ textDecoration: "none" }}>
-          <div className="w-96 h-32 bg-gray-300 m-3 my-24 rounded-md relative hover:shadow-2xl">
-            <div className="p-3">
-              <div className="text-xl">{user.classdesc}</div>
-              <div>{user.sec}</div>
-            </div>
-            <div className="rounded-full bg-green-400 w-16 h-16 absolute left-72 top-12 pl-7 pt-5 text-white">
-              P
-            </div>
-            <div className={`h-48 mt-2 ${user.color} rounded-md`}></div>
-          </div>
-        </NavLink>
-      ))}
-    </div>
-  );
+	return (
+		<div className="container flex flex-wrap">
+			{ClassData.map(class_ => (
+				<NavLink key={class_.id} to="/Class/:id/stream" style={{ textDecoration: "none" }}>
+					<div className="w-96 h-32 bg-gray-300 m-3 my-24 rounded-md relative hover:shadow-2xl">
+						<div className="p-3">
+							<div className="text-xl">{class_.classdesc}</div>
+							<div>{class_.sec}</div>
+						</div>
+						<div className="rounded-full bg-green-400 w-16 h-16 absolute left-72 top-12 pl-7 pt-5 text-white">P</div>
+						<div className={`h-48 mt-2 ${class_.color} rounded-md`} />
+					</div>
+				</NavLink>
+			))}
+		</div>
+	)
 }
--- src/pages/Classes/ClassData.js
+++ src/pages/Classes/ClassData.js
@@ -1,13 +1,8 @@
 export const ClassData = [
-  { classdesc: "Soft Computing", sec: "AIML", color: "bg-green-100" },
-  {
-    classdesc: "Object Oriented Programming",
-    sec: "IOT",
-    color: "bg-yellow-100",
-  },
-  { classdesc: "Data Analytics", sec: "CSIT", color: "bg-red-100" },
-  { classdesc: "Data Analytics", sec: "CSIT", color: "bg-orange-100" },
-  { classdesc: "Data Analytics", sec: "CSIT", color: "bg-blue-100" },
-  { classdesc: "Data Analytics", sec: "CSIT", color: "bg-cyan-100" },
-
-];
+	{ id: 1, classdesc: "Soft Computing", sec: "AIML", color: "bg-green-100" },
+	{ id: 2, classdesc: "Object Oriented Programming", sec: "IOT", color: "bg-yellow-100" },
+	{ id: 3, classdesc: "Data Analytics", sec: "CSIT", color: "bg-red-100" },
+	{ id: 4, classdesc: "Data Analytics", sec: "CSIT", color: "bg-orange-100" },
+	{ id: 5, classdesc: "Data Analytics", sec: "CSIT", color: "bg-blue-100" },
+	{ id: 6, classdesc: "Data Analytics", sec: "CSIT", color: "bg-cyan-100" },
+]

70ab7c7ba498671df7b219c921bcadcf7dc8a37d
warmachine028 <warmachine028@outlook.com>
2023-02-25 18:14:23 +0530

    feat: Classes Fetched dynamically

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { RxHamburgerMenu, RxDoubleArrowLeft, RxPlus } from "react-icons/rx"
-import { NavLink, useLocation, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { useState } from "react"
 import CreateClass from "./createclass"
 import { auth } from "../firebase"
@@ -8,18 +8,19 @@ import { auth } from "../firebase"
 export default function Navbar({ setShowNews, showNews }) {
 	const [pop, setPop] = useState(false)
 
-	const location = useLocation()
 	const navigate = useNavigate()
 	const handlePop = () => {
 		setPop(prev => !prev)
 	}
+	const className = localStorage.getItem("className")
+	const heading = location.pathname === "/classes" ? "EdVenture" : className
 	return (
 		auth.currentUser && (
 			<div className="w-full bg-[#645CBB] h-12 flex text-white pt-2 relative">
 				<div className="px-5 pt-2" onClick={() => setShowNews(!showNews)}>
 					<RxHamburgerMenu className="cursor-pointer" />
 				</div>
-				<div className="text-xl basis-8/12">Classname</div>
+				<div className="text-xl basis-8/12">{heading}</div>
 				<div className={`flex pt-1 ${location.pathname == "/Class" ? "hidden" : "block"}`}>
 					<NavLink to="/Class/:id/stream" style={{ textDecoration: "none" }}>
 						<div className="px-3 hover:text-gray-300 transition duration-500">Stream</div>
--- src/pages/Classes/Class.jsx
+++ src/pages/Classes/Class.jsx
@@ -1,22 +1,60 @@
-import React from "react"
-import { ClassData } from "./ClassData"
+import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
+import { fetchClassesByUser, fetchClassesEnrolled, fetchUserByEmail } from "../../context/DataContext"
+import { auth } from "../../firebase"
+const Class = () => {
+	const [classes, setClasses] = useState([])
+	const [fetching, setFetching] = useState(false)
+	const colors = ["bg-purple-700", "bg-red-700", "bg-green-300"]
+	useEffect(() => () => getClasses(), [])
 
-export default function Class() {
+	const getClasses = async () => {
+		setFetching(true)
+		const { id: userId, name } = await fetchUserByEmail(auth.currentUser?.email)
+		const classesByUser = await fetchClassesByUser(userId)
+		const classesEnrolled = await fetchClassesEnrolled(userId)
+		const classes = []
+		classesByUser.forEach(async class_ => {
+			const randomColor = colors[Math.floor(Math.random() * colors.length)]
+			classes.push({
+				...class_,
+				userName: name,
+				color: randomColor,
+			})
+		})
+		classesEnrolled.forEach(async class_ => {
+			const randomColor = colors[Math.floor(Math.random() * colors.length)]
+			classes.push({
+				...class_,
+				userName: name,
+				color: randomColor,
+			})
+		})
+		setClasses(classes)
+		setFetching(false)
+	}
 	return (
-		<div className="container flex flex-wrap">
-			{ClassData.map(class_ => (
-				<NavLink key={class_.id} to="/Class/:id/stream" style={{ textDecoration: "none" }}>
-					<div className="w-96 h-32 bg-gray-300 m-3 my-24 rounded-md relative hover:shadow-2xl">
-						<div className="p-3">
-							<div className="text-xl">{class_.classdesc}</div>
-							<div>{class_.sec}</div>
+		<div className="container flex flex-wrap max-w-full">
+			{classes.length ? (
+				classes.map(class_ => (
+					<NavLink key={class_.id} to={`/Class/${class_.id}/stream`} style={{ textDecoration: "none" }}>
+						<div className="w-96 h-32 bg-gray-300 m-3 my-24 rounded-md relative hover:shadow-2xl">
+							<div className="p-3">
+								<div className="text-xl">{class_.name}</div>
+								<div>{class_.section}</div>
+							</div>
+							<div className="rounded-full bg-green-400 w-16 h-16 absolute left-72 top-12 pl-7 pt-5 text-white">{class_?.userName[0]}</div>
+							<div className={`h-48 mt-2 ${class_.color} rounded-md`} />
 						</div>
-						<div className="rounded-full bg-green-400 w-16 h-16 absolute left-72 top-12 pl-7 pt-5 text-white">P</div>
-						<div className={`h-48 mt-2 ${class_.color} rounded-md`} />
-					</div>
-				</NavLink>
-			))}
+					</NavLink>
+				))
+			) : fetching ? (
+				<div className=" pl-7 pt-5 w-100 text-black">Fetching Your Classes...</div>
+			) : (
+				<div className=" pl-7 pt-5 w-100 text-black">No classes available</div>
+			)}
 		</div>
 	)
 }
+
+export default Class

e43cc26dcdf8b4e56b6811a657b70b634ed7ffe2
Shreya Jash <shreyajash98@gmail.com>
2023-02-25 15:45:39 +0530

    style: input text color

--- src/components/createclass.jsx
+++ src/components/createclass.jsx
@@ -7,19 +7,19 @@ const CreateClass=(prop)=>{
        <div className="createclasspop-header h-[15%] w-[100%] box-border flex justify-center items-center text-[1.5rem] font-bold">Create Class</div>
        <form className="flex flex-col items-center  justify-evenly h-[70%] w-[100%]">
         <input type="text"
-        className="w-[80%] h-[10%] p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500]  border border-[black] border-solid box-border "
+        className="w-[80%] h-[10%] text-slate-900 p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500]  border border-[black] border-solid box-border "
         placeholder="Class Name"
         />
         <input type="text"
-        className="w-[80%] h-[10%] p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
+        className="w-[80%] h-[10%] text-slate-900  p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
         placeholder="Section"
         />
         <input type="text"
-        className="w-[80%] h-[10%] p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
+        className="w-[80%] h-[10%] text-slate-900 p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
         placeholder="Stream"
         />
         <input type="text"
-        className="w-[80%] h-[10%] p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
+        className="w-[80%] h-[10%] text-slate-900 p-4 placeholder:text-[rgba(0, 0, 0, 0.5)] text-[1.2rem] font-[500] border border-[black] border-solid box-border"
         placeholder="Subject"
         />
        </form>

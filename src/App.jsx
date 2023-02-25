d97028f7199ae8dbe93a215526464b2de561db52
warmachine028 <warmachine028@outlook.com>
2023-02-25 10:21:56 +0530

    refactor: added handleChange Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,47 @@
-import React, { useState }   from 'react'
-import Class from './pages/Classes/Class'
-import Stream from './pages/Classroom/Stream/Stream'
-import People from './pages/Classroom/People/People'
-import Assignment from './pages/Classroom/Assignment/Assignment'
-import Quizzes from './pages/Quiz/Quizzes/Quizzes'
-import Rank from './pages/Quiz/Rank/Rank'
-import Score from './pages/Quiz/Score/Score'
-import Navbar from './components/Navbar'
-import { useLocation } from "react-router-dom";
-import {BrowserRouter as Router, Routes ,Route } from 'react-router-dom'
-
+import React, { useState } from "react"
+import Class from "./pages/Classes/Class"
+import Stream from "./pages/Classroom/Stream/Stream"
+import People from "./pages/Classroom/People/People"
+import Assignment from "./pages/Classroom/Assignment/Assignment"
+import Quizzes from "./pages/Quiz/Quizzes/Quizzes"
+import Rank from "./pages/Quiz/Rank/Rank"
+import Score from "./pages/Quiz/Score/Score"
+import Navbar from "./components/Navbar"
+import { useLocation } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 //Pages
-import Sign_in from './pages/Sign_in'
-import Sign_up from './pages/Sign_up';
-import Sidebar from './components/Sidebar'
-import Modal from './components/Modal'
+import Sign_in from "./pages/Sign_in"
+import Sign_up from "./pages/Sign_up"
+import Sidebar from "./components/Sidebar"
+import Modal from "./components/Modal"
 
 function App() {
-  const [showNews, setShowNews] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+	const [showNews, setShowNews] = useState(false)
+	const [showModal, setShowModal] = useState(false)
 
-  console.log(showNews);
-  return (
-    <div className="App">
-    <Router>
-      <Navbar setShowNews={setShowNews} showNews={showNews} setShowModal={setShowModal} showModal={showModal}/>
-      <Sidebar setShowNews={setShowNews} showNews={showNews} />
-      <Modal setShowModal={setShowModal} showModal={showModal}/>
-      <Routes>
-          <Route path="/" element={<Sign_in/>}/>
-          <Route path="/Sign_up" element={<Sign_up/>}/>
-          <Route path="/Class" element={<Class/>}/>
-          <Route path="/quiz" element={<Quizzes/>}/>
-          <Route path="/quiz/:id" element={<Rank/>}/>
-          <Route path="/score" element={<Score/>}/>
+	return (
+		<div className="App">
+			<Router>
+				<Navbar setShowNews={setShowNews} showNews={showNews} setShowModal={setShowModal} showModal={showModal} />
+				<Sidebar setShowNews={setShowNews} showNews={showNews} />
+				<Modal setShowModal={setShowModal} showModal={showModal} />
+				<Routes>
+					<Route path="/" element={<Sign_in />} />
+					<Route path="/Sign_up" element={<Sign_up />} />
+					<Route path="/Class" element={<Class />} />
+					<Route path="/quiz" element={<Quizzes />} />
+					<Route path="/quiz/:id" element={<Rank />} />
+					<Route path="/score" element={<Score />} />
 
-
-
-
-          <Route path="/Class/:id/stream" element={<Stream/>}/>
-          <Route path="/Class/:id/assignment" element={<Assignment/>}/>
-          <Route path="/Class/:id/people" element={<People/>}/>
-
-        </Routes>
-     </Router>
-    </div>
-    // <Class/>
-  )
+					<Route path="/Class/:id/stream" element={<Stream />} />
+					<Route path="/Class/:id/assignment" element={<Assignment />} />
+					<Route path="/Class/:id/people" element={<People />} />
+				</Routes>
+			</Router>
+		</div>
+		// <Class/>
+	)
 }
 
 export default App
--- src/pages/Sign_in.jsx
+++ src/pages/Sign_in.jsx
@@ -14,47 +14,60 @@ function Sign_in() {
 
 	function handleChange(e) {
 		const { name, value } = e.target
-		console.log(e.target)
 		setUser({
 			...user,
 			[name]: value,
 		})
 	}
+	const handleSubmit = e => {
+		e.preventDefault()
+		console.log("HELLO")
+		console.log(user)
+	}
 	return (
 		<div className="signIn">
-			<h2 className="text-3xl pt-3 pl-6 absolute">Application name</h2>
+			<h2 className="text-3xl pt-3 pl-6 absolute text-white">EdVenture</h2>
 			<img src={signinillustration} className="ill absolute bottom-0 left-0" />
-			<div className="signIn-box"></div>
-			<div className="signIn-container">
-				<div className="sign_in_innerContainer">
-					<h2 className="text-center font-bold text-4xl pb-10">Sign In</h2>
-					<form className="login_page flex flex-col" autoComplete="off">
-						<input type="email" placeholder="Email Address" className="login_input border-b-2 mb-10 pb-2" name="email" value={user.email} onChange={handleChange} />
+			<div className="signIn-box">
+				<div className="signIn-container">
+					<div className="sign_in_innerContainer">
+						<h2 className="text-center font-bold text-4xl pb-10">Sign In</h2>
+						<form className="login_page flex flex-col" autoComplete="off" onSubmit={handleSubmit}>
+							<input //
+								type="email"
+								placeholder="Email Address"
+								className="login_input border-b-2 mb-10 pb-2"
+								name="email"
+								value={user.email}
+								onChange={handleChange}
+							/>
+							<input
+								type="password"
+								className="login_input border-b-2 mb-10 pb-2"
+								placeholder="Password"
+								autoComplete="new-password"
+								name="password"
+								value={user.password}
+								onChange={handleChange}
+							/>
 
-						<input
-							type="password"
-							className="login_input border-b-2 mb-10 pb-2"
-							placeholder="Password"
-							autoComplete="new-password"
-							name="password"
-							value={user.password}
-							onChange={handleChange}
-						/>
+							<button type="submit" className="login_button h-10 rounded-md mb-8 bg-purple-700">
+								Login
+							</button>
+						</form>
+						<p className="text-center pb-5 font-semibold">-OR-</p>
 
-						<button className="login_button h-10 rounded-md mb-8">Login</button>
-					</form>
-					<p className="text-center pb-5 font-semibold">-OR-</p>
+						<button className="google flex h-10 mb-5 p-1 rounded-md">
+							<img src={GoogleIcon} className="googleicon h-7 w-7 pr-1" />
+							<div>Sign in using Google</div>
+						</button>
 
-					<button className="google flex h-10 mb-5 p-1 rounded-md">
-						<img src={GoogleIcon} className="googleicon h-7 w-7 pr-1" />
-						<div>Sign in using Google</div>
-					</button>
-
-					<div className="loginend flex flex-row justify-center">
-						<p className="pr-20">Don't have account?</p>
-						<NavLink to="/Sign_up" className={`navi`}>
-							Sign Up
-						</NavLink>
+						<div className="loginend flex flex-row justify-center ">
+							<p className="pr-20">Don't have account?</p>
+							<NavLink to="/Sign_up" className="navi rounded-md bg-purple-700">
+								Sign Up
+							</NavLink>
+						</div>
 					</div>
 				</div>
 			</div>

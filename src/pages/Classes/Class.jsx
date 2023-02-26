913a2cecbeb053daa6a74c5351ea88527bcc0a02
warmachine028 <warmachine028@outlook.com>
2023-02-26 17:24:59 +0530

    fix: added try catch

--- src/pages/Classes/Class.jsx
+++ src/pages/Classes/Class.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import { fetchClassesByUser, fetchClassesEnrolled, fetchUserByEmail } from "../../context/DataContext"
 import { auth } from "../../firebase"
 const Class = () => {
+	console.log("in Classes")
 	const [classes, setClasses] = useState([])
 	const [fetching, setFetching] = useState(false)
 	const colors = ["bg-purple-700", "bg-red-700", "bg-green-500", "bg-blue-900", "bg-green-600", "bg-amber-400", "bg-cyan-400"]
@@ -10,6 +11,7 @@ const Class = () => {
 	const navigate = useNavigate()
 	const getClasses = async () => {
 		setFetching(true)
+		console.log("useEffect Initiated")
 		const { id: userId, name } = await fetchUserByEmail(auth.currentUser?.email)
 		const classesByUser = await fetchClassesByUser(userId)
 		const classesEnrolled = await fetchClassesEnrolled(userId)
@@ -42,14 +44,12 @@ const Class = () => {
 			{classes.length ? (
 				classes.map(class_ => (
 					<div className="w-96 h-32 bg-gray-300 m-3 my-24 rounded-md relative hover:shadow-2xl cursor-pointer" onClick={() => handleClick(class_)} key={class_.id}>
-						<div style={{ display: "flex", justifyContent:"space-between" }}>
+						<div style={{ display: "flex", justifyContent: "space-between" }}>
 							<div className="p-3">
 								<div className="text-xl">{class_.name}</div>
 								<div>{class_.section}</div>
 							</div>
-							<div className="p-3 text-2xl pr-10">
-								⚙️
-							</div>
+							<div className="p-3 text-2xl pr-10">⚙️</div>
 						</div>
 						<div className="rounded-full bg-green-400 w-16 h-16 absolute left-60 sm:left-72 top-12 pl-7 pt-5 text-white">{class_?.userName[0]}</div>
 						<div className={`h-48 mt-2 ${class_.color} rounded-md hover:shadow-2xl`} />

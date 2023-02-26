43cea1cfce35ca512d9469683609c6b8f3dd0500
vishaaal <officialvk02@gmail.com>
2023-02-26 08:13:50 +0530

    button create assignment

--- src/pages/Classroom/Assignment/Assignment.jsx
+++ src/pages/Classroom/Assignment/Assignment.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import note from "../../../assets/images/note.png";
 import paperclip from "../../../assets/images/paperclip.png"
 import {AssignData} from "./assignment_data";
+import { AiOutlinePlus} from 'react-icons/Ai';
 
 
 export default function Assignment() {
   return (
     <div className='container p-5'>
+        <button type="button" class="text-white bg-[#645CBB] hover:bg-[#645CBB]/90 focus:ring-4 focus:outline-none focus:ring-bg-[#645CBB]/50 font-medium rounded-full text-sm px-7 py-2.5 text-center inline-flex items-center dark:focus:ring-[#645CBB]/55 mr-2 mb-2 ml-1 pl-5">
+  <AiOutlinePlus className='mr-2'/>
+  Create
+</button>
         {AssignData.map((info) => (
         <div className="h-36 bg-pink-100 my-7 rounded-md flex pt-7 pl-6">
         <div className="bg-green-400 h-20 w-20 p-3 rounded-full">

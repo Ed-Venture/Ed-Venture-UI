cc004536e63c3bd35a852f2e4ed0fa8ae15e5694
warmachine028 <warmachine028@outlook.com>
2023-02-26 17:30:10 +0530

    fix: added try catch

--- src/pages/Classes/Class.jsx
+++ src/pages/Classes/Class.jsx
@@ -8,37 +8,43 @@ const Class = () => {
 	const [fetching, setFetching] = useState(false)
 	const colors = ["bg-purple-700", "bg-red-700", "bg-green-500", "bg-blue-900", "bg-green-600", "bg-amber-400", "bg-cyan-400"]
 	const navigate = useNavigate()
-	const getClasses = async () => {
-		setFetching(true)
-		console.log("useEffect Initiated")
-		const { id: userId, name } = await fetchUserByEmail(auth.currentUser?.email)
-		const classesByUser = await fetchClassesByUser(userId)
-		const classesEnrolled = await fetchClassesEnrolled(userId)
-		const classes = []
-		classesByUser.forEach(async class_ => {
-			const randomColor = colors[Math.floor(Math.random() * colors.length)]
-			classes.push({
-				...class_,
-				userName: name,
-				color: randomColor,
-			})
-		})
-		classesEnrolled.forEach(async class_ => {
-			const randomColor = colors[Math.floor(Math.random() * colors.length)]
-			classes.push({
-				...class_,
-				userName: name,
-				color: randomColor,
-			})
-		})
-		setClasses(classes)
-		setFetching(false)
-	}
+	// const getClasses = async () => {
+
+	// }
 	const handleClick = class_ => {
 		localStorage.setItem("class", JSON.stringify(class_))
 		navigate(`/classes/${class_.id}/stream`)
 	}
-	useEffect(() => () => getClasses(), [])
+	// useEffect(() => () => getClasses(), [])
+	useEffect(() => {
+		return async () => {
+			setFetching(true)
+			console.log("useEffect Initiated")
+			const { id: userId, name } = await fetchUserByEmail(auth.currentUser?.email)
+			const classesByUser = await fetchClassesByUser(userId)
+			const classesEnrolled = await fetchClassesEnrolled(userId)
+			const classes = []
+			classesByUser.forEach(async class_ => {
+				const randomColor = colors[Math.floor(Math.random() * colors.length)]
+				classes.push({
+					...class_,
+					userName: name,
+					color: randomColor,
+				})
+			})
+			classesEnrolled.forEach(async class_ => {
+				const randomColor = colors[Math.floor(Math.random() * colors.length)]
+				classes.push({
+					...class_,
+					userName: name,
+					color: randomColor,
+				})
+			})
+			setClasses(classes)
+			setFetching(false)
+		}
+	}, [])
+
 	return (
 		<div className="container flex flex-wrap max-w-full">
 			{classes.length ? (
